733820bf1dd9c3ec7984a1183c6673e99d26fd9f
omnya zerghouni <omnyazer@gmail.com>
2025-01-10 14:06:42 +0100

    questions mélangées

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -12,7 +12,7 @@ import {
   loadFromLocalStorage,
   saveToLocalStorage,
   startTimer,
-  shuffleArray, // Import de la fonction shuffleArray
+  shuffleArray,
 } from "./utils.js";
 
 console.log("Quiz JS loaded...");
@@ -57,13 +57,19 @@ const timeLeftSpan = getElement("#time-left");
 const currentQuestionIndexSpan = getElement("#current-question-index");
 const totalQuestionsSpan = getElement("#total-questions");
 
+// Bouton et icône pour le mode sombre
+const darkModeToggle = getElement("#dark-mode-toggle");
+const darkModeIcon = getElement("#dark-mode-icon");
+
 // Init
 startBtn.addEventListener("click", startQuiz);
 nextBtn.addEventListener("click", nextQuestion);
 restartBtn.addEventListener("click", restartQuiz);
+darkModeToggle.addEventListener("click", toggleDarkMode);
 
 setText(bestScoreValue, bestScore);
 
+// Fonction pour démarrer le quiz
 function startQuiz() {
   hideElement(introScreen);
   showElement(questionScreen);
@@ -79,6 +85,7 @@ function startQuiz() {
   showQuestion();
 }
 
+// Fonction pour afficher une question
 function showQuestion() {
   clearInterval(timerId);
 
@@ -105,6 +112,7 @@ function showQuestion() {
   );
 }
 
+// Fonction pour sélectionner une réponse
 function selectAnswer(index, btn) {
   clearInterval(timerId);
 
@@ -121,6 +129,7 @@ function selectAnswer(index, btn) {
   nextBtn.classList.remove("hidden");
 }
 
+// Fonction pour passer à la question suivante
 function nextQuestion() {
   currentQuestionIndex++;
   if (currentQuestionIndex < questions.length) {
@@ -130,6 +139,7 @@ function nextQuestion() {
   }
 }
 
+// Fonction pour terminer le quiz
 function endQuiz() {
   hideElement(questionScreen);
   showElement(resultScreen);
@@ -143,9 +153,42 @@ function endQuiz() {
   setText(bestScoreEnd, bestScore);
 }
 
+// Fonction pour redémarrer le quiz
 function restartQuiz() {
   hideElement(resultScreen);
   showElement(introScreen);
 
   setText(bestScoreValue, bestScore);
+}
+
+function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle("dark-mode");
+
+  // Ajoute une transition pour le fondu
+  const iconElement = darkModeIcon;
+  iconElement.classList.add("fade");
+
+  if (body.classList.contains("dark-mode")) {
+    iconElement.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path fill="#ffda00" d="M18 12a6 6 0 1 1-12 0a6 6 0 0 1 12 0"/>
+        <path fill="#ffda00" fill-rule="evenodd" d="M12 1.25a.75.75 0 0 1 .75.75v1a.75.75 0 0 1-1.5 0V2a.75.75 0 0 1 .75-.75M4.399 4.399a.75.75 0 0 1 1.06 0l.393.392a.75.75 0 0 1-1.06 1.061l-.393-.393a.75.75 0 0 1 0-1.06m15.202 0a.75.75 0 0 1 0 1.06l-.393.393a.75.75 0 0 1-1.06-1.06l.393-.393a.75.75 0 0 1 1.06 0M1.25 12a.75.75 0 0 1 .75-.75h1a.75.75 0 0 1 0 1.5H2a.75.75 0 0 1-.75-.75m19 0a.75.75 0 0 1 .75-.75h1a.75.75 0 0 1 0 1.5h-1a.75.75 0 0 1-.75-.75m-2.102 6.148a.75.75 0 0 1 1.06 0l.393.393a.75.75 0 1 1-1.06 1.06l-.393-.393a.75.75 0 0 1 0-1.06m-12.296 0a.75.75 0 0 1 0 1.06l-.393.393a.75.75 0 1 1-1.06-1.06l.392-.393a.75.75 0 0 1 1.061 0M12 20.25a.75.75 0 0 1 .75.75v1a.75.75 0 0 1-1.5 0v-1a.75.75 0 0 1 .75-.75" clip-rule="evenodd"/>
+      </svg>
+    `;
+  } else {
+    iconElement.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path fill="#284d92" d="M20.958 15.325c.204-.486-.379-.9-.868-.684a7.7 7.7 0 0 1-3.101.648c-4.185 0-7.577-3.324-7.577-7.425a7.3 7.3 0 0 1 1.134-3.91c.284-.448-.057-1.068-.577-.936C5.96 4.041 3 7.613 3 11.862C3 16.909 7.175 21 12.326 21c3.9 0 7.24-2.345 8.632-5.675"/>
+        <path fill="#284d92" d="M15.611 3.103c-.53-.354-1.162.278-.809.808l.63.945a2.33 2.33 0 0 1 0 2.588l-.63.945c-.353.53.28 1.162.81.808l.944-.63a2.33 2.33 0 0 1 2.588 0l.945.63c.53.354 1.162-.278.808-.808l-.63-.945a2.33 2.33 0 0 1 0-2.588l.63-.945c.354-.53-.278-1.162-.809-.808l-.944.63a2.33 2.33 0 0 1-2.588 0z"/>
+      </svg>
+    `;
+  }
+
+  setTimeout(() => {
+    iconElement.classList.remove("fade");
+  }, 300);
+
+  const isDarkMode = body.classList.contains("dark-mode");
+  saveToLocalStorage("darkMode", isDarkMode);
 }
